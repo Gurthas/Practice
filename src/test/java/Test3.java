afe77183fb1d43238983a8c1d24e94d400c0308f
Gurthas <357380758@qq.com>
2015-11-19 13:30:34 +0800

    ADD

---
+++ src/test/java/Test3.java
@@ -0,0 +1,27 @@
+/*判断101-200之间有多少个素数，并输出所有素数*/
+package test;
+import java.util.*;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int count=0;
+         for(int i=101;i<200;i++){
+        	   boolean b= true;
+        	   //求质数，只需除到它的平方根
+               for(int j=2;j<=Math.sqrt(i);j++){
+            	  if(i%j==0){
+            	  b =false;
+         }
+          
+        	 }
+               if(b==true){
+            	   count++;
+            	   System.out.println("Count:"+count+"-----"+i);
+            	  
+               }
+         }
+	}
+
+}
