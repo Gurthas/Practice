98cc9163d9d535f56744ff969d700f4e2b934464
Gurthas <357380758@qq.com>
2016-01-13 18:02:54 +0800

    HttpClient Practice

---
+++ src/test/java/test/Tpp.java
@@ -0,0 +1,15 @@
+package testbaidu;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+
+public class Tpp
+{
+public static void main(String[] args) throws ClientProtocolException, IOException
+{
+	Httpclientt hc = new Httpclientt();
+	hc.byGetMethod();
+			
+}
+}
