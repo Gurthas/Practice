8f384b84082053d76b518b6815713cfeec9e81b5
Gurthas <357380758@qq.com>
2016-03-04 17:23:57 +0800

    修改GetVerifyCode

--- src/test/python/ApiList/GetVerifyCode.py
+++ src/test/python/ApiList/GetVerifyCode.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # coding: utf-8
 import unittest
 import requests
@@ -25,21 +24,3 @@ class GetVerifyCode():
     login = requests.post(url,data)
     login1 = json.loads(login.text)
     use_server_token = login1['data']['server_token']
-=======
-import unittest
-from ApiList import SharkClient
-from api_test.ApiList import _DEVICE_ID, _BB
-import requests
-
-class GetVerifyCode(unittest.TestCase):
-    def setUp(self):
-        self.client = SharkClient('debug')
-    def test_verifycode(self):
-        url = 'http://dev.shark.kuaikuaiyu.com/api.login?platform=android&version=0,21'
-        res = self.client.login_sms('15011431186')
-        verify_code = self.client.query_verify_code('15011431186')
-        print verify_code
-        
-        
-        
->>>>>>> 6d26d4e8ac6616982f841b8c4aa3822084f01046
