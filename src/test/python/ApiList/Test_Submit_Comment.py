09c5ecfc7f3a98ea0ea6add4981141067473bedf
Gurthas <357380758@qq.com>
2016-03-07 15:09:48 +0800

    优化

--- src/test/python/ApiList/Test_Submit_Comment.py
+++ src/test/python/ApiList/Test_Submit_Comment.py
@@ -2,31 +2,20 @@
 import unittest
 import requests
 from qqqq.ApiList import SharkClient
-from qqqq.GetVerifyCode import GetVerifyCode
+import time
+import hashlib
 
 
 class Test_Submit_Comment(unittest.TestCase):
     def setUp(self):
         self.client = SharkClient('debug')
+        res0 = self.client.login('15011431186')
         print 'start...'
-    def test_submit_comment(self):
-        res = self.client.submit_comment('5583b16f38a2124d5c799368',GetVerifyCode.use_server_token, '我凑屌啊', 'good', GetVerifyCode.user_token,'56a1d83c1ad1d400018af8d8')
-        print res
-        '''
-        url = 'http://dev.shark.kuaikuaiyu.com/api.order.comment.submit?platform=android&version=0,21' 
-        data = {
-            'uid':'5583b16f38a2124d5c799368',
-            'server_token':GetVerifyCode.use_server_token,
-            'comment':'卧槽屌哦',
-            'comment_type':'good',
-            'user_token':GetVerifyCode.user_token,
-            'oid':'56a1d9711ad1d400018af8ed',
-            }
-        response = requests.post(url,data)
-        print GetVerifyCode.use_server_token
-        print response.text
-        '''
         
+    def test_submit_comment(self):
+        res = self.client.submit_comment('我凑屌啊','good','569efb3cc7949e00015336a4')
+        print res
+
     def tearDown(self):
         print '*****************************************'
         print 'destroyed...'   
