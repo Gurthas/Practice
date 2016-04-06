72b176e188e2ae036d9a96206ff2d13b876b1e40
yupeng <ypyupeng@icloud.com>
2016-04-06 22:45:43 +0800

    fix some dependence problem

--- src/main/java/com/learnjavaee/JdbcConnection.java
+++ src/main/java/com/learnjavaee/JdbcConnection.java
@@ -1,66 +1,75 @@
-package com.firstweb;
-import java.sql.*;
+package com.learnjavaee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class JdbcConnection {
-	private static String userName;
-	private static String password;
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public static boolean validate() {
-		String url = "jdbc:mysql://localhost:3306/testmy1?characterEncoding=utf-8&useSSL=false";
-		String user = "root";
-		String dbpassword = "123456";
-		boolean result = false;
+    private static String userName;
+    private static String password;
 
-		try {
-			String driver = "com.mysql.jdbc.Driver";
-			Class.forName(driver);
-			Connection con = DriverManager.getConnection(url, user, dbpassword);
+    public String getUserName() {
+        return userName;
+    }
 
-			if (con == null) {
-				System.out.println("can't open DBConnection");
-			}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-			String sql = "select * from users where username =? and password=?";
+    public String getPassword() {
+        return password;
+    }
 
-			PreparedStatement pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, userName);
-			pstmt.setString(2, password);
-			System.out.println(pstmt);
-			ResultSet rs = pstmt.executeQuery();
-			if (rs != null) {
-				if (rs.next()) {
-					System.out.println("Welcome!");
-					result = true;
-					while (rs.next())
-						System.out.println("username:" + rs.getString(2)
-								+ "  password:" + rs.getString(7));
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-				} else {
-					System.out.println("Failed");
-					result = false;
-				}
-				
-			}
-			
-			pstmt.close();
-			rs.close();
-			con.close();
+    public static boolean validate() {
+        String url = "jdbc:mysql://localhost:3306/testmy1?characterEncoding=utf-8&useSSL=false";
+        String user = "root";
+        String dbpassword = "123456";
+        boolean result = false;
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
+        try {
+            String driver = "com.mysql.jdbc.Driver";
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, user, dbpassword);
 
-	}
+            if (con == null) {
+                System.out.println("can't open DBConnection");
+            }
+
+            String sql = "select * from users where username =? and password=?";
+
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, userName);
+            pstmt.setString(2, password);
+            System.out.println(pstmt);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs != null) {
+                if (rs.next()) {
+                    System.out.println("Welcome!");
+                    result = true;
+                    while (rs.next())
+                        System.out.println("username:" + rs.getString(2)
+                                + "  password:" + rs.getString(7));
+
+                } else {
+                    System.out.println("Failed");
+                    result = false;
+                }
+
+            }
+
+            pstmt.close();
+            rs.close();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
 }
--- src/main/java/com/learnjavaee/Login.java
+++ src/main/java/com/learnjavaee/Login.java
@@ -1,20 +1,19 @@
-package com.firstweb;
+package com.learnjavaee;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.firstweb.JdbcConnection;
+import java.io.IOException;
 
 /**
  * Servlet implementation class Login
  */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -23,39 +22,36 @@ public class Login extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getParameter("username");
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getParameter("username");
         request.getParameter("password");
-        request.getSession().setAttribute("users", request.getParameter("username"));      //½«users·ÅÔÚAttributeÖÐ
-        JdbcConnection user1=new JdbcConnection();
+        request.getSession().setAttribute("users", request.getParameter("username"));      //ï¿½ï¿½usersï¿½ï¿½ï¿½ï¿½Attributeï¿½ï¿½
+        JdbcConnection user1 = new JdbcConnection();
         user1.setUserName(request.getParameter("username"));
         user1.setPassword(request.getParameter("password"));
-        System.out.println(request.getParameter("username")+request.getParameter("password"));
-        
-        if(user1.validate())
-        {
-          
-            request.getRequestDispatcher("welcome.jsp").forward(request, response); //Ð£ÑéÓÃ»§ÃûÃÜÂëÕýÈ·£¬Ìø×ªµ½welcome.jsp
-             
+        System.out.println(request.getParameter("username") + request.getParameter("password"));
+
+        if (user1.validate()) {
+
+            request.getRequestDispatcher("welcome.jsp").forward(request, response); //Ð£ï¿½ï¿½ï¿½Ã»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È·ï¿½ï¿½ï¿½ï¿½×ªï¿½ï¿½welcome.jsp
+
+        } else {
+            request.getRequestDispatcher("index.jsp").forward(request, response);  //Ð£ï¿½ï¿½ï¿½Ã»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ë²»ï¿½ï¿½È·ï¿½ï¿½ï¿½ï¿½×ªï¿½ï¿½index.jsp
+
         }
-        else
-        {
-            request.getRequestDispatcher("index.jsp").forward(request, response);  //Ð£ÑéÓÃ»§ÃûÃÜÂë²»ÕýÈ·£¬Ìø×ªµ½index.jsp
-            
-        }
-        
-   }
-	}
+
+    }
+}
 
 
