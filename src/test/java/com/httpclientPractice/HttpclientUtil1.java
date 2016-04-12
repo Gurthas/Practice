f42b2c01715e2ff398ef7868248a0a79d9edd0db
yupeng <ypyupeng@icloud.com>
2016-04-12 23:09:13 +0800

    change post method

--- src/test/java/com/httpclientPractice/HttpclientUtil1.java
+++ src/test/java/com/httpclientPractice/HttpclientUtil1.java
@@ -1,4 +1,15 @@
-package com.httpclient.practice;
+package com.httpclientPractice;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.net.ProtocolException;
@@ -8,44 +19,35 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-
 
 public class HttpclientUtil1 {
-	public static String post(String url, Map<String, String> params)
-			throws IOException, ProtocolException {
-		String httpstr = null;
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		String body = null;
-		HttpPost post = new HttpPost(url);
-		List<NameValuePair> list = new ArrayList<NameValuePair>(params.size());
-		for (Entry<String, String> entry : params.entrySet()) {
-			NameValuePair pair = new BasicNameValuePair(entry.getKey(), entry
-					.getValue().toString());
-			list.add(pair);
-			UrlEncodedFormEntity uefentity = new UrlEncodedFormEntity(list,
-					StandardCharsets.UTF_8);
-			post.setEntity(uefentity);
-
-			CloseableHttpResponse response = httpclient.execute(post);
-			HttpEntity entity = response.getEntity();
-			httpstr = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-			System.out.println(httpstr);
-			if (entity != null) {
-				System.out.println(response.toString());
-				httpclient.close();
-
-			}
-		}
-
-		return httpstr;
-	}
+    public static String post(String url, Map<String, String> params)
+            throws IOException, ProtocolException {
+        String httpstr = null;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        String body = null;
+        HttpPost post = new HttpPost(url);
+        List<NameValuePair> list = new ArrayList<NameValuePair>(params.size());
+        for (Entry<String, String> entry : params.entrySet()) {
+            NameValuePair pair = new BasicNameValuePair(entry.getKey(), entry
+                    .getValue().toString());
+            list.add(pair);
+            UrlEncodedFormEntity uefentity = new UrlEncodedFormEntity(list,
+                    StandardCharsets.UTF_8);
+            post.setEntity(uefentity);
+        }
+        try {
+            CloseableHttpResponse response = httpclient.execute(post);
+            HttpEntity entity = response.getEntity();
+            httpstr = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } finally {
+            if (httpclient != null)
+                httpclient.close();
+        }
+        return httpstr;
+    }
 }
