e5b63a5fbe2150756f5ec617ba044dc76ce37304
Gurthas <357380735@qq.com>
2016-04-12 22:57:15 +0800

    封装httpclient post方法

---
+++ src/test/java/com/httpclientPractice/Testshop.java
@@ -0,0 +1,13 @@
+package com.httpclient.practice;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Testshop {
+	public static void main(String[] args) throws Exception {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("area_id", "566e800738a2127e2686f9f8");
+		String url = "http://dev.shark.kuaikuaiyu.com/api.shop.list?platform=android&version=0,21&did=5978ad54b1c2033d";
+		HttpclientUtil1.post(url, params);
+
+	}
+}
